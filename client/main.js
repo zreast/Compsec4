7f1bcc13c0197d92da79d02bebdd8dfe257afa81
Kittitad Seelawiang <kisaragishinraijin@outlook.co.th>
2017-11-17 23:31:42 +0700

    Finally, encrypted file is working!

--- client/main.js
+++ client/main.js
@@ -1,22 +1,35 @@
 angular.module('fileUpload', ['ngFileUpload'])
 .controller('MyCtrl',['Upload','$window',function(Upload,$window){
     var vm = this;
+    var key = 'hello?';
+    var salt = CryptoJS.lib.WordArray.random(128/8);
+    var iv = CryptoJS.lib.WordArray.random(128/8);
+
     vm.submit = function(){ //function to call on form submit
         if (vm.upload_form.file.$valid && vm.file) { //check if form is valid
-            console.log(vm.file);
-            vm.upload(vm.file); //call upload function
+            var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(vm.file), key, {
+                iv: iv,
+                mode: CryptoJS.mode.CBC,
+                padding: CryptoJS.pad.Pkcs7
+            });
+            var encryptedFile = new File([ciphertext], vm.file.name + '.encrypted', {
+                type: vm.file.type,
+                lastModified: vm.file.lastModified
+            });
+            vm.upload(encryptedFile);
         }
     }
 
     vm.upload = function (file) {
+        console.log(file);
         Upload.upload({
             url: 'http://localhost:3000/upload', //webAPI exposed to upload the file
-            data:{file:file} //pass file as data, should be user ng-model
+            data: {file: file} //pass file as data, should be user ng-model*/
         }).then(function (resp) { //upload function returns a promise
             if(resp.data.error_code === 0){ //validate success
-                $window.alert('Success ' + resp.config.data.file.name + ' uploaded. Response: ');
+                $window.alert('Success ' + resp.config.data.file.name + ' uploaded.');
             } else {
-                $window.alert('an error occured');
+                $window.alert('An error occured.');
             }
         }, function (resp) { //catch error
             console.log('Error status: ' + resp.status);
@@ -27,5 +40,5 @@ angular.module('fileUpload', ['ngFileUpload'])
             console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
             vm.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
         });
-    };
+    }
 }]);
